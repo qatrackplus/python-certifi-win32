5faa756d221b904997f36f69d7f05910dcd0578f
Andrew Leech <andrew@alelec.net>
2017-03-22 04:39:16 +0000

    Update setup.py to add long_description

--- setup.py
+++ setup.py
@@ -4,11 +4,15 @@ import os
 from setuptools import setup
 from distutils.sysconfig import get_python_lib
 
-setup_kwargs = dict(
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    long_description = readme.read()
+
+setup(
     name = 'python-certifi-win32',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     description = 'Add windows certificate store to certifi cacerts.',
+    long_description=long_description,
     author = 'Andrew Leech',
     author_email = 'andrew.leech@planetinnovation.com.au',
     license = 'BSD',
@@ -18,5 +22,3 @@ setup_kwargs = dict(
     data_files = [(get_python_lib(prefix=''), ['python-certifi-win32-init.pth'])],
     install_requires = ['wrapt>=1.10.4', 'wincertstore', 'certifi'],
 )
-
-setup(**setup_kwargs)
